442aa7248b8adbbea78cdf6c58f10bbcf17fee46
Wallychi <szetrov@gmail.com>
2025-04-16 11:34:14 +0100

    add more code to js

--- script.js
+++ script.js
@@ -14,7 +14,24 @@ console.log("Javascript")
 // console.log(false&&true)
 // console.log(false&&false)
 // console.log(true&&true)
-console.log(false||false)
-console.log(!true)
-console.log(!false)
-console.log(!true&&true||false)
+// console.log(false||false)
+// console.log(!true)
+// console.log(!false)
+// console.log(!true&&true||false)
+// console.log(5=='5')
+console.log('5'==='5')
+console.log(5!='5')
+console.log(5!=='5');
+// (5<2) ? console.log('5 is less than 2') : console.log('5 is greater than 2')
+// console.log((5<2) ? '5 is less than 2' : '5 is greater than 2')
+// let firstName
+// console.log(firstName)
+// console.log(typeof(firstName))
+// let firstName
+// firstName='Precious'
+// console.log(firstName)
+let firstName = 'Precious'
+firstName='LightSeid'
+console.log(firstName)
+const lastName = 'Ebuka'
+console.log(lastName)
